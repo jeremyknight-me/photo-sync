21a862c2b33aa6a640d51e6f984ff38345db3a8a
Jeremy <jeremy@theknights.me>
2021-02-19 23:52:37 -0600

    completed basic layout

---
+++ src/PhotoSync/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+using PhotoSync.Commands;
+
+namespace PhotoSync.ViewModels
+{
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.ExitCommand = new ShutdownCommand();
+        }
+
+        public ICommand ExitCommand { get; private set; }
+    }
+}
