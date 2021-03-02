191719308530d9ab7b5bfff39b0edd47902a1f9f
Jeremy <jeremy@theknights.me>
2021-03-02 17:50:39 -0600

    added photos

---
+++ src/PhotoSync.Data/Entities/Photo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace PhotoSync.Data.Entities
+{
+    public class Photo
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string RelativePath { get; set; }
+        public bool IsOrphaned { get; set; } = false;
+        public bool IsNew { get; set; } = false;
+        public DateTimeOffset DateCreated { get; private set; } = DateTimeOffset.Now;
+    }
+
+    internal class PhotoConfig : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.ToTable("Photo");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.RelativePath).IsRequired();
+
+            builder.Property(x => x.DateCreated)
+                .HasConversion(new DateTimeOffsetToStringConverter());
+        }
+    }
+}
---
+++ src/PhotoSync.Data/PhotoSyncContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhotoSync.Data.Entities;
+
+namespace PhotoSync.Data
+{
+    public class PhotoSyncContext : DbContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
