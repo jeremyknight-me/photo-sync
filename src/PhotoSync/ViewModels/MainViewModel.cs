6f159a6869482dc44620a2cbc3a384663f77aa3a
Jeremy <jeremy@theknights.me>
2021-03-03 23:14:52 -0600

    added selected library and photo properties

--- src/PhotoSync/ViewModels/MainViewModel.cs
+++ src/PhotoSync/ViewModels/MainViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using PhotoSync.Commands;
 using PhotoSync.Models;
 
 namespace PhotoSync.ViewModels
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
+        private string selectedLibrary;
+        private string selectedPhoto;
+
         public MainViewModel()
         {
             this.ExitCommand = new ShutdownCommand();
@@ -15,9 +20,48 @@ namespace PhotoSync.ViewModels
             this.PhotoActionOptions = new ObservableCollection<KeyValuePair<int, string>>(PhotoActionHelper.MakeEnumerable());
         }
 
-        public ICommand ExitCommand { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public ObservableCollection<KeyValuePair<int, string>> PhotoActionOptions { get; set; }
+        public ICommand ExitCommand { get; private set; }
+        public ICommand NewCommand { get; private set; }
+
+        public ObservableCollection<KeyValuePair<int, string>> PhotoActionOptions { get; private set; }
+
         public int SelectedPhotoAction { get; set; }
+
+        public string SelectedLibrary
+        {
+            get => string.IsNullOrWhiteSpace(this.selectedLibrary)
+                ? "No Sync Database Selected..."
+                : this.selectedLibrary;
+            private set
+            {
+                if (this.selectedLibrary != value)
+                {
+                    this.selectedLibrary = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string SelectedPhoto
+        {
+            get => string.IsNullOrWhiteSpace(this.selectedPhoto)
+                ? "No Photo Selected..."
+                : this.selectedPhoto;
+            private set
+            {
+                if (this.selectedPhoto != value)
+                {
+                    this.selectedPhoto = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        // This method is called by the Set accessor of each property.  
+        // The CallerMemberName attribute that is applied to the optional propertyName  
+        // parameter causes the property name of the caller to be substituted as an argument.  
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
