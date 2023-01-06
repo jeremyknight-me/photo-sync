40a816089e04f0c7d5fb7ecd8987e063e919adbe
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-01-06 16:31:10 -0600

    added dependency injection

--- src/PhotoSync/App.xaml.cs
+++ src/PhotoSync/App.xaml.cs
@@ -1,7 +1,31 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PhotoSync;
 
 public partial class App : Application
 {
+    private readonly ServiceProvider serviceProvider;
+
+    public App()
+    {
+        var serviceCollection = new ServiceCollection();
+        this.ConfigureServices(serviceCollection);
+        this.serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    internal T GetService<T>() => this.serviceProvider.GetService<T>();
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var mainWindow = this.serviceProvider.GetService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
 }
