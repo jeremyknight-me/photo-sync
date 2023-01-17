3bfb0676af1bd4663af5125bf6de1c1983f04e58
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-01-17 11:53:00 -0600

    displayed list of folders in treeview

---
+++ src/PhotoSync/Models/LibraryFolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PhotoSync.Models;
+
+public sealed class LibraryFolder
+{
+    public string Name { get; set; }
+    public string FullPath { get; set; }
+    public string RelativePath { get; set; }
+    public bool IsExcluded { get; set; }
+    public List<LibraryFolder> Children { get; set; } = new();
+}
--- src/PhotoSync/ViewModels/LibraryViewModel.cs
+++ src/PhotoSync/ViewModels/LibraryViewModel.cs
@@ -1,12 +1,65 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.Generic;
+using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
 using PhotoSync.Domain;
+using PhotoSync.Domain.Extensions;
+using PhotoSync.Models;
 
 namespace PhotoSync.ViewModels;
 
 [INotifyPropertyChanged]
 public partial class LibraryViewModel
 {
+    [ObservableProperty]
+    private List<LibraryFolder> folders = new();
+
+    [ObservableProperty]
+    private string libraryFileName;
+
+    [ObservableProperty]
+    private string libraryDestinationFolder;
+
     public PhotoLibrary Library { get; private set; }
 
-    public void SetLibrary(PhotoLibrary library) => this.Library = library;
+    public void SetLibrary(PhotoLibrary library)
+    {
+        this.Library = library;
+        this.LibraryDestinationFolder = library.DestinationFolder;
+        this.LibraryFileName = library.FileName;
+        this.LoadTreeFolders();
+    }
+
+    private void LoadTreeFolders()
+    {
+        var path = this.Library.SourceFolder;
+        this.Folders.Clear();
+
+        if (!Directory.Exists(path))
+        {
+            return;
+        }
+
+        var root = new DirectoryInfo(path);
+        this.Folders = this.CreateFolderItems(root);
+    }
+
+    private List<LibraryFolder> CreateFolderItems(DirectoryInfo directoryInfo)
+    {
+        var folderItems = new List<LibraryFolder>();
+        foreach (var directory in directoryInfo.GetDirectories())
+        {
+            var relativePath = this.Library.GetPathRelativeToSource(directoryInfo.FullName);
+            var folder = new LibraryFolder
+            {
+                Name = directory.Name,
+                FullPath = directory.FullName,
+                RelativePath = relativePath,
+                IsExcluded = this.Library.ExcludedFolders.Exists(relativePath),
+                Children = this.CreateFolderItems(directory)
+            };
+            folderItems.Add(folder);
+        }
+
+        return folderItems;
+    }
 }
